8fdf592d4bf135b08b6935aff32735414adb8ef4
Yuriy <39201286+yuri2711@users.noreply.github.com>
2023-01-27 22:09:16 +0300

    hw03-reflection: Добавлен readme.md (#4)

---
+++ hw04-gc/src/main/java/ru/otus/Calculator.java
@@ -0,0 +1,28 @@
+package ru.otus;
+
+import java.time.LocalDateTime;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int counter = 100_000_000;
+        var summator = new Summator();
+        long startTime = System.currentTimeMillis();
+
+        for (var idx = 0; idx < counter; idx++) {
+            var data = new Data(idx);
+            summator.calc(data);
+
+            if (idx % 10_000_000 == 0) {
+                System.out.println(LocalDateTime.now() + " current idx:" + idx);
+            }
+        }
+
+        long delta = System.currentTimeMillis() - startTime;
+        System.out.println(summator.getPrevValue());
+        System.out.println(summator.getPrevPrevValue());
+        System.out.println(summator.getSumLastThreeValues());
+        System.out.println(summator.getSomeValue());
+        System.out.println(summator.getSum());
+        System.out.println("spend msec:" + delta + ", sec:" + (delta / 1000));
+    }
+}
