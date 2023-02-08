4a3981189da24d4dcb37867e01a8e3933d27bc35
Юрий <79991621127@yandex.ru>
2023-02-08 13:51:39 +0300

    hw13-DI: homework

---
+++ hw13-DI/src/main/java/ru/otus/appcontainer/config_v2/AppConfig2.java
@@ -0,0 +1,16 @@
+package ru.otus.appcontainer.config_v2;
+
+import ru.otus.appcontainer.api.AppComponent;
+import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+import ru.otus.services.*;
+
+@AppComponentsContainerConfig(order = 2)
+public class AppConfig2 {
+
+    @AppComponent(order = 2, name = "gameProcessor")
+    public GameProcessor gameProcessor(IOService ioService,
+                                       PlayerService playerService,
+                                       EquationPreparer equationPreparer) {
+        return new GameProcessorImpl(ioService, equationPreparer, playerService);
+    }
+}
