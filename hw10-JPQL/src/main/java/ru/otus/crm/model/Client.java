69f94f0067d864fd29dbce05bcb5e7a66cdd4389
Yuriy <39201286+yuri2711@users.noreply.github.com>
2023-02-08 11:46:36 +0300

    JPQL-homeWork (#10)

---
+++ hw10-JPQL/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,84 @@
+package ru.otus.crm.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "client")
+    private List<Phone> phones;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.setPhones(phones);
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name, this.address, this.phones);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Phone> getPhones() {
+        return phones != null ? phones : new ArrayList<>();
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+
+        if (this.phones != null) {
+            this.phones.forEach(ph -> ph.setClient(this));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                ", phones=" + phones +
+                '}';
+    }
+}
